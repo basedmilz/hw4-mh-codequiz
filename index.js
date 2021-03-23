10a74f0d464438a726b9b8ae3f1bd1850715d724
basedmilz <basedmilz@gmail.com>
2021-03-23 10:02:18 -0400

    added timer

--- index.js
+++ index.js
@@ -1,29 +1,82 @@
-var timeEl = document.querySelector(".time");
-var mainEl = document.getElementById("main");
+var button = document.querySelector(".button");
+var timeLeft = 0;
+var timerEl = document.getElementById("timeLeft");
+var animeQuestions = [
+    {
+        question: "What animal is Naruto's Jinchuriki?",
+        choices:
+            ["A: Fox",
+                "B: Rabbit",
+                "C: Snake",
+                "D: Monkey"],
+
+        answer: "A: Fox"
+
+    },
+    {
+        question: "What is Monster's main villian name?",
+        choices:
+            ["A: Detective Lunge ",
+                "B: Johan Liebert",
+                "C: Eva Heinemann",
+                "D: Ana Liebert"],
+
+        answer: "B: Johan Liebert"
+    },
+    {
+        question: "What is the name of Yusuke's special ability?",
+        choices:
+            ["A: Eraser Cannon",
+                "B: Galick Gun",
+                "C: Spirit Gun",
+                "D: Spirit Sword"],
+
+        answer: "C: Spirit Gun"
+    },
+    {
+        question: "What is Eren's Titan From called?",
+        choices:
+            ["A: Armored Titan",
+                "B: Founding Titan",
+                "C: Colossal Titan",
+                "D: Attack Titan"],
+
+        answer: "D: Attack Titan"
+    },
+    {
+        question: "What is Ash's starter Pokemon?",
+        choices:
+            ["A: Charmander ",
+                "B: Piplup ",
+                "C: Pikachu ",
+                "D: Eevee "],
+
+        answer: "C: Pikachu"
+    }]
 
 
-function setTime() {
-    // Sets interval in variable
-    var timerInterval = setInterval(function() {
-      secondsLeft--;
-      timeEl.textContent = secondsLeft + " seconds left till colorsplosion.";
-  
-      if(secondsLeft === 0) {
-        // Stops execution of action at set interval
-        clearInterval(timerInterval);
-        // Calls function to create and append image
-        sendMessage();
-      }
-  
+function startQuiz() {
+    startTimer()
+
+
+}
+
+function startTimer() {
+    timeLeft = 75;
+    timer = setInterval(function () {
+        if (timeLeft > 1) {
+            timerEl.textContent = timeLeft + ' seconds remaining';
+            timeLeft--;
+        }
+
+        
+
+       
+
+
     }, 1000);
-  }
-  // Function to create and append colorsplosion image
-function sendMessage() {
-    timeEl.textContent = " ";
-    var imgEl = document.createElement("img");
-    imgEl.setAttribute("src", "images/image_1.jpg");
-    mainEl.appendChild(imgEl);
-  
-  }
-  
-  setTime();
+}
+
+
+
+button.addEventListener("click", startQuiz);
